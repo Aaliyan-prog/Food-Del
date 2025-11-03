1bfb5d3a13c2b9c3754efff3e98d79bc8094d9f7
Aaliyan ahmed <my.inventory786@gmail.com>
2025-11-03 19:13:39 +0500

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://aaliyandev:558849623845@cluster0.penkudb.mongodb.net/food-del").then(() => console.log("db connected"))
+  await mongoose.connect("your-mongo-url").then(() => console.log("db connected"))
 }
